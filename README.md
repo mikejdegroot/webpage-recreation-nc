dab04414950057fe27ac00b6aab9d3525f614bdd
Mike de Groot <mike.j.degroot@gmail.com>
2017-10-09 20:18:10 +0100

    fully completed

---
+++ README.md
@@ -0,0 +1,18 @@
+### Newcross Webpage recreation
+
+Notes
+
+- You must have gulp CLI installed globally, it is compiling the SCSS
+- Download the repo and type 'npm i' to download the packages
+
+
+Extra things
+
+- A simple regex controls the validations of the form, and is hidden when the input is correct and resubmitted
+
+
+Future thoughts
+- Some of the spacing and lettering is a bit off when the browser window is resized (rather than using the mobile chrome tool), if I had more time I would look into polishing this up a bit.
+
+
+Thats it, thank you!   
--- js/app.js
+++ js/app.js
@@ -1,1 +1,31 @@
-console.log('JS loaded');
+$(() => {
+  console.log('JS loaded');
+
+  const $button = $('.button');
+  const $emailInput = $('.emailInput');
+  const $emailWarn = $('.emailWarn');
+  const $passInput = $('.passInput');
+  const $passWarn = $('.passWarn');
+
+
+
+  $button.on('click', () => {
+    if (!$emailInput.val().match(/\S+@\S+\.\S+/)) {
+      $emailWarn.html('Not a valid email address');
+      $emailWarn.css('list-style-image', `url('./assets/icon-error.svg')`);
+
+    } else {
+      $emailWarn.html('');
+      $emailWarn.css('list-style-image', `url()`);
+    }
+
+    if($passInput.val().length < 7) {
+      $passWarn.html('password must be at least 7 characters');
+      $passWarn.css('list-style-image', `url('./assets/icon-info.svg')`);
+
+    } else {
+      $passWarn.html('');
+      $passWarn.css('list-style-image', `url()`);
+    }
+  });
+});
